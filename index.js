befece772f61273eafcc0b44a4715aa087b68733
hardc0l2e <ronartos@gmail.com>
2022-06-06 20:47:15 +0800

    updates

--- index.js
+++ index.js
@@ -137,9 +137,7 @@ const run = async (
   } 
   else if (statistic.startsWith("Custom")) {
     const where = condition;
-    sql = `select ${db.sqlsanitize(statistic)}(${db.sqlsanitize(
-           field
-     )}) as the_stat from ${schema}"${tbl.name}" ${where}`;
+    sql = `$(where)`;
  }  
    else
     sql = `select ${db.sqlsanitize(statistic)}(${db.sqlsanitize(
