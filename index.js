6cc364c1c2f91d92799d0be53d84170fe2b75947
hardc0l2e <ronartos@gmail.com>
2022-06-06 21:09:44 +0800

    updates

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ const run = async (
   } 
   else if (statistic.startsWith("Custom")) {
     const where = condition;
-    sql = `${condition}`;
+    sql = condition;
  }  
    else
     sql = `select ${db.sqlsanitize(statistic)}(${db.sqlsanitize(
